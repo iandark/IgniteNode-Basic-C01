43845278e6c0f4e159842d8e7a43edd4c31ce88f
Derkian Souza <derkian@iands.com.br>
2021-03-10 06:52:01 -0300

    feat: Inclusão de projeto funcional

---
+++ readme.md
@@ -0,0 +1,24 @@
+# Ignite - Node - Chapter 01
+
+### Requisitos
+
+- [x] Deve ser possível criar uma conta
+- [x] Deve ser possível buscar o extrato bancário do cliente
+- [x] Deve ser possível realizar um depósito
+- [x] Deve ser possível realizar um saque
+- [x] Deve ser possível buscar o extrato bancário do cliente por data
+- [x] Deve ser possível atualizar dados da conta do cliente
+- [x] Deve ser possível obter dados da conta do cliente
+- [x] Deve ser possível deletar uma conta
+- [x] Deve ser possível retornar o balance
+
+### Regras de negócio
+
+- [x] Não deve ser possível cadastrar uma conta com CPF já existente
+- [x] Não deve ser possivel buscar extrato em uma conta não existente
+- [x] Não deve ser possível fazer depósito em uma conta não existente
+- [x] Não deve ser possível fazer saque em uma conta não existente
+- [x] Não deve ser possível fazer saque quando o saldo for insuficiente
+- [x] Não deve ser possível excluir uma conta não existente
+
+Collection (Postman): [https://www.getpostman.com/collections/fda52f603294e717462f](https://www.getpostman.com/collections/fda52f603294e717462f)
---
+++ src/index.js
@@ -0,0 +1,149 @@
+const express = require('express');
+const { v4: uuidV4 } = require('uuid');
+
+const app = express();
+app.use(express.json());
+
+const customers = [];
+
+const accountByCPF = function (req, res, next) {
+    const { cpf } = req.headers;
+    const customer = customers.find(customer => customer.cpf === cpf);
+
+    if(!customer) {
+        return res.json({ message: "Customer don't exist"}).status(400);
+    }
+
+    req.customer = customer;
+    next();
+};
+
+const getBalance = function (statement) {
+    const balance = statement.reduce((acc, operation) => {
+        if(operation.type=== "CREDIT") {
+            return acc += operation.amount;
+        } else {
+            return acc -= operation.amount;
+        }
+    }, 0);
+
+    return balance;
+};
+
+const validateDate = function (date) {
+    const dateReg = /[0-9]{2}[/][0-9]{2}[/][0-9]{4}$/;
+    const validDate = Array.isArray(date.match(dateReg));
+
+    return validDate;
+}
+
+app.post('/account', (req, res)=>{
+    const { cpf, name } = req.body;
+
+    const id = uuidV4();
+
+    const customerAlreadyExist = customers.some(customer=> customer.cpf  === cpf)
+
+    if(customerAlreadyExist) {
+        return res.json({message: "There's a customer already in database with this CPF"}).status(401);
+    }
+
+    customers.push({
+        id,
+        cpf,
+        name,
+        statement: []
+    })
+    res.sendStatus(201);
+});
+
+app.use(accountByCPF);
+
+app.get('/statement', (req, res)=>{
+    const { statement } = req.customer;
+    return res.json(statement);
+
+});
+
+app.post('/deposit', (req, res)=>{
+    const { description, amount } = req.body;
+
+    const statement = {
+        description,
+        amount,
+        created_at: new Date(),
+        type: "CREDIT"
+    }
+
+    req.customer.statement.push(statement);
+
+    res.sendStatus(201);
+
+});
+
+app.post('/withdraw', (req, res)=>{
+    const { amount } = req.body;
+
+    const balance = getBalance(req.customer.statement);
+
+    if(amount>balance) {
+        return res.json({ message: "Insufficient founds"}).status(401);
+    }
+
+    const statement = {
+        amount,
+        created_at: new Date(),
+        type: "DEBIT"
+    }
+    req.customer.statement.push(statement);
+
+    res.sendStatus(201);
+});
+
+
+app.get('/statement/date', (req, res)=>{
+    const { date } = req.body;
+    const { statement } = req.customer;
+
+    if(!validateDate(date)) {
+        return res.json({ message: "Invalid date"}).status(401);
+    }
+
+    const arrDate = date.split('/');
+    const dateFilter = new Date(arrDate[2], arrDate[1]-1, arrDate[0]);
+
+    const statementDateFilter = [];
+
+    statement.filter(operation => {
+        if(dateFilter <= operation.created_at) {
+            statementDateFilter.push(operation);
+        }
+    })
+
+    return res.json(statementDateFilter);
+});
+
+app.put('/account', (req, res)=>{
+    const { name } = req.body;
+    const { customer } = req;
+    customer.name = name;
+
+    return res.sendStatus(201);
+});
+
+app.get('/account', (req, res)=>{
+    const { customer } = req;
+
+    return res.json(customer);
+});
+
+app.delete('/account', (req, res)=>{
+    const { customer } = req;
+
+    customers.splice(customers.indexOf(customer), 1);
+    res.json(customers).status(200);
+});
+
+app.listen(3333, ()=>{
+    console.log('Listening on port 3333');
+});
